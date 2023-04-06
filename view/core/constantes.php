1b5211d30a140d271eb1b618eb0aac753dafdde6
Jesús I.V <90335295+JesusAIV@users.noreply.github.com>
2023-04-06 18:10:50 -0500

    Actualizando titulo

--- view/core/constantes.php
+++ view/core/constantes.php
@@ -1,2 +1,9 @@
 <?php
+
+    /*
+    // HOSTING
+    const SERVERURL = "https://tgestiona.friendsdevaj.com/";
+    */
     const SERVERURL = "http://localhost:8085/CRUDTRPHP/";
+
+    const NAME = "TGestiona";
